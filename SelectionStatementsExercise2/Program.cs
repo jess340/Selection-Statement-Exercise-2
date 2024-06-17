c7599e62feb9be4cca807470bc7f380a92335176
“Jessica <osias.jessica@gmail.comgit config --global user.email osias.jessica@gmail.com>
2024-06-17 17:21:24 -0400

    exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math": 
+                    Console.WriteLine("Math is tough subject!");
+                    break;
+
+                case "english":
+                Console.WriteLine("English is an interesting subject!");
+                break;
+
+                case "science":
+                    Console.WriteLine("Science is an awesome subject!");
+                    break;
+
+                default:
+                    Console.WriteLine($"Ah wow! I haven't taken that subject before. {subject} has been on my mind to take for some time now!");
+                    break; 
+           
+            }
+
+          
         }
     }
 }
